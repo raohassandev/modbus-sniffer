079f2b10be93111ce4af523d3d72f8b5abd6445a
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-16 15:00:35 +0500

    v8: export digital-twin services

--- src/v8/index.js
+++ src/v8/index.js
@@ -45,6 +45,8 @@ module.exports = Object.freeze({
   ...require('./traffic/trafficTimelineService'),
   ...require('./traffic/registerLabService'),
   ...require('./traffic/trafficRoutes'),
+  ...require('./digitalTwin/digitalTwinService'),
+  ...require('./digitalTwin/digitalTwinRoutes'),
   ...require('./testCenter/rawFrameStudio'),
   ...require('./testCenter/recipeEngine'),
   ...require('./testCenter/testCenterWorkspaceService'),
