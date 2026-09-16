2e67b901db812e80580d4535a5b6411d8b178a10
RAO Hassan <139758709+raohassandev@users.noreply.github.com>
2026-09-16 15:03:48 +0500

    v8 WP-16: expose JavaScript SDK entrypoint

---
+++ sdk/js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const { WorkbenchApiClient, WorkbenchClientError } = require('../../src/v8/automation/workbenchClient');
+
+module.exports = {
+  WorkbenchApiClient,
+  WorkbenchClientError,
+};
